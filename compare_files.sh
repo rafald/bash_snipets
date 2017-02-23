cb97c3ad122f69896026fa440519b7039cf89075
rafald <rdzbek@gmail.com>
2017-02-23 10:50:46 +0100

    Create compare_files.sh

---
+++ compare_files.sh
@@ -0,0 +1,11 @@
+
+
+cmp - compare two files byte by byte
+  cmp : this command is used to compare two files byte by byte and as any mismatch occurs,it echoes it on the screen.
+  
+diff - compare files line by line
+
+comm - compare two SORTED files line by line
+     find out the records available in one but not in another
+
+sdiff - side-by-side merge of file differences
